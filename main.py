03411e1f22811e0fd58558341637815a75017665
Hani_Sul <141285108+Hnoooy007@users.noreply.github.com>
2025-10-23 04:52:09 +0300

    main.py

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import telebot
 import time
 import os
 
+# توكن البوت
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['count10'])
-def count_10(message):
+# عد من 10 إلى 0
+@bot.message_handler(commands=['عد'])
+def عد10(message):
     for i in range(10, -1, -1):
         bot.send_message(message.chat.id, str(i))
         time.sleep(1)
 
-@bot.message_handler(commands=['count5'])
-def count_5(message):
+# عد من 5 إلى 0
+@bot.message_handler(commands=['عد5'])
+def عد5(message):
     for i in range(5, -1, -1):
         bot.send_message(message.chat.id, str(i))
         time.sleep(1)
 
+# تشغيل البوت
 bot.polling()
